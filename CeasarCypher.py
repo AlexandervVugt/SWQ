af2b5b0c7ab0e702e4e88632b8b8f3950bc0d0bb
Alexander <alexandervvugt@gmail.com>
2021-10-23 15:42:23 +0200

    reduced duplicate code in ceasar cypher

--- CeasarCypher.py
+++ CeasarCypher.py
@@ -2,18 +2,13 @@ def increment(salt):
     salt += 1
     return salt%128
 
-def encrypt(content: str):
+def cypher(content: str, encrypt: bool):
     salt = 1
     result = ""
     for char in content:
-        result += chr(ord(char) + salt)
-        salt = increment(salt)
-    return result
-
-def decrypt(content: str):
-    salt = 1
-    result = ""
-    for char in content:
-        result += chr(ord(char) - salt)
+        if encrypt:
+            result += chr(ord(char) + salt)
+        else:
+            result += chr(ord(char) - salt)
         salt = increment(salt)
     return result
